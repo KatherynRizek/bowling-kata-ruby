5f168e9871da384f7f678740bc6459b237630e21
Katheryn Osmond <katheryn.osmond@excella.com>
2018-08-02 17:31:17 +0000

    Game works for spares

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,13 +1,33 @@
 class Game
     def initialize()
         @score = 0
+        @frame_scores = Array.new(21, 0)
+        @current_roll = 0
+        @isSpare = false
     end
 
     def roll(pins)
-        @score += pins
+        @frame_scores[@current_roll] = pins
+        @current_roll += 1
     end
 
+
     def score()
-        return @score
+        scoreSum = 0
+        prevScore = 0
+        @frame_scores.each do |scoreNow|
+            scoreNow = scoreNow.to_i
+            if @isSpare
+                scoreNow = scoreNow * 2
+                @isSpare = false
+            end
+            if prevScore + scoreNow == 10
+                @isSpare = true
+            end
+            scoreSum += scoreNow
+            prevScore = scoreNow
+        end
+        return scoreSum
     end
+
 end
--- tests/test_bowling_game.rb
+++ tests/test_bowling_game.rb
@@ -24,7 +24,16 @@ class TestBowlingGame < Test::Unit::TestCase
   end
 
   def test_roll_one_spare
-
+    g = Game.new()
+    g.roll(7)
+    g.roll(3) #adds up to spare
+    g.roll(5)
+    i = 0
+    until i >= 17 do
+      g.roll(0)
+      i += 1
+    end
+    assert_equal(20, g.score)
   end
 
 end
