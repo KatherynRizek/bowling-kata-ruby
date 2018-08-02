9c8f663aee65e9b58c71be0aa78a146676240231
Katheryn Osmond <katheryn.osmond@excella.com>
2018-08-02 18:38:46 +0000

    Refactored spare code

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -14,20 +14,27 @@ class Game
 
     def score()
         scoreSum = 0
-        prevScore = 0
+        i = 0
         @frame_scores.each do |scoreNow|
             scoreNow = scoreNow.to_i
-            if @isSpare
-                scoreNow = scoreNow * 2
-                @isSpare = false
+            if (i >= 2) 
+                if (self.isSpare(@frame_scores[i-1].to_i, @frame_scores[i-2].to_i)) == true
+                    scoreNow = scoreNow * 2
+                end
             end
-            if prevScore + scoreNow == 10
-                @isSpare = true
-            end
+            i += 1
             scoreSum += scoreNow
-            prevScore = scoreNow
         end
         return scoreSum
+    end
+
+    def isSpare(previous_score, two_previous_score)
+        #spares can only be calculated on 3rd and beyond roll of frame
+        if (@current_roll % 2 == 0) && (previous_score + two_previous_score == 10)
+            return true
+        else
+            return false
+        end
     end
 
 end
--- tests/test_bowling_game.rb
+++ tests/test_bowling_game.rb
@@ -36,4 +36,18 @@ class TestBowlingGame < Test::Unit::TestCase
     assert_equal(20, g.score)
   end
 
+  def test_roll_two_spares
+    g = Game.new()
+    g.roll(7)
+    g.roll(3) #adds up to spare
+    g.roll(8)
+    g.roll(2) #adds up to two spares
+    g.roll(3)
+    i = 0
+    until i >= 15 do
+      g.roll(0)
+      i += 1
+    end
+    assert_equal(34, g.score)
+  end
 end
