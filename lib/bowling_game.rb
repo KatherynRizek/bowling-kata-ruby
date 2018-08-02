c317a124ea0e2b5598cbc1542de541ab12dacaf8
Katheryn Osmond <katheryn.osmond@excella.com>
2018-08-02 20:16:12 +0000

    Added scoring for strikes

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -9,18 +9,26 @@ class Game
     def roll(pins)
         @frame_scores[@current_roll] = pins
         @current_roll += 1
+        if self.isStrike(@frame_scores[@current_roll-1], @frame_scores.length - 1)
+            @current_roll += 1
+        end
     end
 
 
     def score()
         scoreSum = 0
         i = 0
+        isNotFirstStrike = false
         @frame_scores.each do |scoreNow|
             scoreNow = scoreNow.to_i
-            if (i >= 2) 
-                if (self.isSpare(@frame_scores[i-1].to_i, @frame_scores[i-2].to_i)) == true
-                    scoreNow = scoreNow * 2
+            if (i >= 2) && self.isSpare(@frame_scores[i-1].to_i, @frame_scores[i-2].to_i, i)
+                scoreNow = scoreNow * 2
+            elsif (i >= 4) && self.isStrike(@frame_scores[i-4].to_i, i)
+                scoreNow = @frame_scores[i-1].to_i  + @frame_scores[i-2].to_i
+                if isNotFirstStrike
+                    scoreNow += 10
                 end
+                isNotFirstStrike = true
             end
             i += 1
             scoreSum += scoreNow
@@ -28,12 +36,28 @@ class Game
         return scoreSum
     end
 
-    def isSpare(previous_score, two_previous_score)
+    def isSpare(previous_score, two_previous_score, frameNum)
         #spares can only be calculated on 3rd and beyond roll of frame
-        if (@current_roll % 2 == 0) && (previous_score + two_previous_score == 10)
+        if (frameNum % 2 == 0) && (previous_score + two_previous_score == 10) && (previous_score != 0)
             return true
         else
             return false
+        end
+    end
+
+    def isStrike(previous_score, frameNum)
+        if (frameNum % 2 == 0) && (previous_score == 10)
+            return true
+        else
+            return false
+        end
+    end
+
+    def manyRolls(numRolls)
+        i = 0
+        until i >= numRolls do
+          self.roll(0)
+          i += 1
         end
     end
 
--- tests/test_bowling_game.rb
+++ tests/test_bowling_game.rb
@@ -5,11 +5,7 @@ class TestBowlingGame < Test::Unit::TestCase
 
   def test_gutter_ball_game
     g = Game.new()
-    i = 0
-    until i >= 20 do
-      g.roll(0)
-      i += 1
-    end
+    g.manyRolls(20)
     assert_equal(0, g.score)
   end
 
@@ -28,11 +24,7 @@ class TestBowlingGame < Test::Unit::TestCase
     g.roll(7)
     g.roll(3) #adds up to spare
     g.roll(5)
-    i = 0
-    until i >= 17 do
-      g.roll(0)
-      i += 1
-    end
+    g.manyRolls(17)
     assert_equal(20, g.score)
   end
 
@@ -43,11 +35,48 @@ class TestBowlingGame < Test::Unit::TestCase
     g.roll(8)
     g.roll(2) #adds up to two spares
     g.roll(3)
-    i = 0
-    until i >= 15 do
-      g.roll(0)
-      i += 1
-    end
+    g.manyRolls(15)
     assert_equal(34, g.score)
   end
+
+  def test_one_strike_beginning
+    g = Game.new()
+    g.roll(10)
+    g.roll(1)
+    g.roll(8)
+    g.manyRolls(16)
+    assert_equal(28, g.score)
+  end
+
+  def test_two_strikes
+    g = Game.new()
+    g.roll(10)
+    g.roll(1)
+    g.roll(7)
+    g.roll(10)
+    g.roll(2)
+    g.roll(3)
+    g.manyRolls(12)
+    assert_equal(46, g.score())
+  end
+
+  def test_one_strike_later
+    g = Game.new()
+    g.roll(1)
+    g.roll(2)
+    g.roll(1)
+    g.roll(7)
+    g.roll(10)
+    g.roll(2)
+    g.roll(3)
+    g.manyRolls(12)
+    assert_equal(31, g.score())
+  end
+
+  # def test_all_strikes
+  #   g = Game.new()
+  #   i = 2
+  #   until 
+
+
 end
